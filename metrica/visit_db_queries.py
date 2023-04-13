b898b236304fd495d3780c4cb50e51e38f2c6d33
Artem.I <klinok066@gmail.com>
2023-04-13 15:27:11 +0500

    refactor db, added methods get_all_ip_by_dates(), get_all_visits_by_ip_and_dates()

--- metrica/visit_db_queries.py
+++ metrica/visit_db_queries.py
@@ -3,10 +3,10 @@ import re
 
 
 class Visit:
-    def __init__(self, ip_address, user_agent, datetime):
+    def __init__(self, ip_address, user_agent, date_time):
         self.ip_address = ip_address
         self.user_agent = user_agent
-        self.datetime = datetime
+        self.date_time = date_time
 
 
 def create_visit_table():
@@ -41,56 +41,65 @@ def add_visit(header: str):
 
 def get_all_visits() -> list:
     visits = IPVisit.select()
-    list_of_visit = []
+    list_of_visitors = []
     for line in visits:
         ip_address = IP.get(id=line.ip_id).ip_address
-        visitor = Visit(ip_address, line.user_agent, line.data_time)
-        list_of_visit.append(visitor)
-    return list_of_visit
+        visitor = Visit(ip_address, line.user_agent, line.date_time)
+        list_of_visitors.append(visitor)
+    return list_of_visitors
 
 
 def get_all_visits_by_ip(ip_address: str) -> list:
     """
 
     """
-    list_of_visits = []
+    list_of_visitors = []
     ip = IP.get(IP.ip_address == ip_address)
     visits = IPVisit.select().where(ip.id == ip.id)
     for line in visits:
-        visitor = Visit(ip_address, line.user_agent, line.data_time)
-        list_of_visits.append(visitor)
-    return list_of_visits
+        visitor = Visit(ip_address, line.user_agent, line.date_time)
+        list_of_visitors.append(visitor)
+    return list_of_visitors
 
 
-def get_all_ip_by_dates(data_time_start: datetime, data_time_end=datetime.now()) -> dict[str, list]:
+def get_all_ip_by_dates(date_time_start: datetime, date_time_end=datetime.now()) \
+        -> dict[str, list]:
     """
     Функция принимает две даты начала отсчета и конца
-    Возвращает словарь где ключ ip клиента, а значение list с временем посещения клиента в заданном промежутке времени
-    :param data_time_start: datatime
-    :param data_time_end: datatime
-    :return: {str(ip):[datatime]}
+    Возвращает
+    :param date_time_start: datatime
+    :param date_time_end: datatime
+    :return:
     """
-    ips_and_date = {}
+    list_of_visitors = []
     date = []
     all_visit = IPVisit.select()
     for visit in all_visit:
-        if data_time_start <= visit.data_time <= data_time_end:
-            date.append(visit.data_time)
-        ip = IP.get(IP.id == visit.ip_id)
-        ips_and_date[ip.ip_address] = date
-
-    return ips_and_date
+        if date_time_start <= visit.date_time <= date_time_end:
+            date.append(visit.date_time)
+        ip_address = IP.get(IP.id == visit.ip_id)
+        visitor = Visit(ip_address, visit.user_agent, visit.date_time)
+        list_of_visitors.append(visitor)
+    return list_of_visitors
 
 
 def get_all_visits_by_ip_and_dates(ip_address: str, date_time_start: datetime, date_time_end=datetime.now()) -> list:
     """
-    Функция принимает ip_address клиента, две даты (начала и конца отсчета). Возвращает list со всеми датами посещения сайта этого клиента.
+    Функция принимает ip_address клиента, две даты (начала и конца отсчета).
+    Возвращает
     :param ip_address:
     :param date_time_start:
     :param date_time_end:
-    :return: list of datetime
+    :return:
     """
-
+    list_of_visitors = []
+    ip = IP.get(IP.ip_address == ip_address)
+    all_visits = IPVisit.select().where(ip.id == ip.id)
+    for visit in all_visits:
+        if date_time_start <= visit.date_time <= date_time_end:
+            visitor = Visit(ip_address, visit.user_agent, visit.date_time)
+            list_of_visitors.append(visitor)
+    return list_of_visitors
 
 
 def visit_close_connection():
